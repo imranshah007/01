a7616e93a95670e592f0363c383379deb4319a10
Imran Shah <is9149933764@gmail.com>
2022-11-03 10:40:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>About Us</h1>
+     <p className="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, neque?</p>
+     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa mollitia labore quia possimus eligendi sapiente, facere reiciendis ad ipsa vero at recusandae est iure voluptates eius! Iste at sint molestiae quos eaque, aliquam ab sapiente dicta fugiat commodi inventore nulla repellendus. Accusamus aperiam recusandae quos pariatur provident cupiditate id placeat architecto, repudiandae fuga distinctio qui doloribus laboriosam dignissimos debitis, reprehenderit voluptate dolor, ducimus laudantium alias esse voluptates ut quibusdam illum! Cupiditate, facere suscipit tenetur accusamus veniam necessitatibus doloremque consectetur quod aspernatur consequuntur provident, quaerat delectus, animi eveniet incidunt nesciunt. Temporibus necessitatibus nostrum ipsum quos cupiditate est quisquam asperiores recusandae magnam!</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum dignissimos voluptate aliquid quae veritatis.</p>
     </div>
   );
 }
